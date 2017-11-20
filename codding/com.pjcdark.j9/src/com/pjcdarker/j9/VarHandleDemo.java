863a1a8106498e75ee98bb29bb6ecc37f99c494a
pjcdarker <pjcdarker@gmail.com>
2017-11-21 00:24:55 +0800

    ...VarHandle demo

--- codding/com.pjcdark.j9/src/com/pjcdarker/j9/VarHandleDemo.java
+++ codding/com.pjcdark.j9/src/com/pjcdarker/j9/VarHandleDemo.java
@@ -1,5 +1,6 @@
 package com.pjcdarker.j9;
 
+import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.VarHandle;
 import java.util.Arrays;
@@ -10,12 +11,35 @@ import java.util.Arrays;
  */
 public class VarHandleDemo {
 
-
-    public static void arrayElementVarHandle() {
+    public void arrayElementVarHandle() {
         String[] sa = new String[]{"abc"};
         VarHandle avh = MethodHandles.arrayElementVarHandle(String[].class);
         boolean r = avh.compareAndSet(sa, 0, "abc", "d");
         System.out.println(r);
         System.out.println(Arrays.toString(sa));
     }
+
+    public MethodHandle toMethodHandle() throws NoSuchFieldException, IllegalAccessException {
+        VarHandle vh = MethodHandles.lookup()
+                                    .in(Eoo.class)
+                                    .findVarHandle(Eoo.class, "s", String.class);
+
+        VarHandle.AccessMode am = VarHandle.AccessMode.valueFromMethodName(VarHandle.AccessMode.GET_AND_SET.methodName());
+        MethodHandle mh = vh.toMethodHandle(am);
+        return mh;
+    }
+
+    public static void main(String[] args) throws Throwable {
+        VarHandleDemo vh = new VarHandleDemo();
+        MethodHandle mh = vh.toMethodHandle();
+        Eoo e = new Eoo();
+        Object ss = mh.invoke(e, "get");
+        System.out.println(ss);
+    }
+
+}
+
+class Eoo {
+    int i;
+    String s;
 }
