854e260d7a138e60a325d341a0ea2bf05ebdb70d
pengjiancong <pjcdarker@gmail.com>
2025-03-02 13:25:01 +0800

    refactor: move base module to core/base

---
+++ core/build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    id("java-conventions")
+}
+
+repositories {
+    mavenCentral()
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,3 @@
 rootProject.name = "java-tour"
-include("base")
+include("core")
+include("core:base")
